2ee94e5d61d83b5a8898c8c1c73e1c9638eb007d
chenbin.li <1015761882@qq.com>
2022-08-31 22:47:47 +0800

    UI:完善login页面

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 const userInfo = {
-    username:'',
-    password:''
+    username: '',
+    password: ''
 }
 </script>
 <template>
-    <div class="h-full flex flex-1 justify-center">
-        <el-form label-position="right" class="card w-1/2 p-4 mt-10">
-            <el-form-item label="Username" required>
-                <el-input v-model="userInfo.username"/>
+    <div class="h-full flex flex-1 flex-col  justify-center">
+        <div class="w-1/2 mx-auto px-4">
+            <router-link to="/">
+                <span class="cursor-pointer hover:text-purple-400">
+                    <el-icon size="20px">
+                        <ArrowLeft />
+                    </el-icon>
+                </span>
+            </router-link>
+        </div>
+        <el-form label-position="right" class="card w-1/2 mx-auto p-4 mt-2">
+            <el-form-item required>
+                <el-input v-model="userInfo.username" placeholder="Username" />
             </el-form-item>
-            <el-form-item label="Password" required>
-                <el-input type="password" v-model="userInfo.password" autocomplete="off"/>
+            <el-form-item required>
+                <el-input type="password" v-model="userInfo.password" placeholder="Password" autocomplete="off" />
             </el-form-item>
             <el-form-item>
-                <button>Sign in</button><button>No account,Sign up</button>
+                <div class="flex justify-between w-full">
+                    <button class="bg-purple-400 text-white">Sign in</button>
+                    <button class="bg-gray-200">No account ? Sign up</button>
+                </div>
             </el-form-item>
         </el-form>
     </div>
