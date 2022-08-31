da302d0589c3c51b1945c794e2aec365cb50b601
chenbin.li <lichenbin1119@gmail.com>
2022-08-31 16:12:20 +0800

    配置跨域

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port:3001,
+    proxy:{
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/,'')
+      }
+    }
+  },
   plugins: [vue()],
   css: {
     preprocessorOptions: {
