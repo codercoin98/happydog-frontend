523adb2997d1e91df380b6d92f3da9afbb3fb846
chenbin.li <lichenbin1119@gmail.com>
2022-09-07 11:46:04 +0800

    ui:调整tools布局样式

--- src/pages/FunnyTools/index.vue
+++ src/pages/FunnyTools/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p class="text-center my-10 text-lg">Some funny tools for you!</p>
-        <ul class="grid-container grid grid-cols-4">
+        <ul class="grid-container grid grid-cols-4 place-items-stretch space-x-4">
             <li v-for="item in tools" :key="item.title">
                 <router-link :to="item.to" class="text-black hover:text-purple-400">
                     <ToolCard :title="item.title" :iconUrl="item.iconUrl" />
@@ -23,6 +23,11 @@ to: '/'
 title: '图书检索',
 iconUrl: 'http://124.220.47.26:7777/images/dog.jpg',
 to: '/search-books'
+},
+{
+title: 'Github用户检索',
+iconUrl: 'http://124.220.47.26:7777/images/dog.jpg',
+to: '/'
 },
 {
 title: 'Github用户检索',
