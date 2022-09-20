9a587ebfd097931ab176e26fe1db94a00bbd9e73
chenbin.li <1015761882@qq.com>
2022-09-20 22:27:29 +0800

    ui:调整左侧按钮样式

--- src/pages/AppLeft/index.vue
+++ src/pages/AppLeft/index.vue
@@ -14,14 +14,14 @@
       <li class="w-52">
         <router-link to="/sign-in">
           <div
-            class="rounded-full text-center border border-purple-400 w-full px-4 py-2 mb-2 cursor-pointer text-purple-400 font-semibold hover:border-purple-500 hover:text-white hover:bg-purple-500 active:bg-purple-400 active:text-white active:border-opacity-0">
+            class="rounded-lg text-center border border-purple-400 w-full px-4 py-2 mb-2 cursor-pointer text-purple-400 font-semibold hover:border-purple-500 hover:text-white hover:bg-purple-500 active:bg-purple-400 active:text-white active:border-opacity-0">
             登录
           </div>
         </router-link>
       </li>
       <li v-for="item in leftList" :key="item.title"
-        :class="route.path === item.path ? 'bg-purple-500 rounded-full text-white' : null"
-        class="flex justify-between w-52 items-center px-4 py-2 cursor-pointer text-black font-semibold hover:bg-purple-500 hover:rounded-full hover:text-white"
+        :class="route.path === item.path ? 'rounded-lg text-purple-400' : null"
+        class="flex justify-between w-52 items-center px-4 py-2 cursor-pointer text-black font-semibold hover:bg-gray-200 hover:rounded-lg group"
         @click="router.push({path:item.path}) ">
         <n-icon size="20" class="mr-4">
           <Home v-if="item.key === 1" />
@@ -30,7 +30,7 @@
           <Tools v-else-if="item.key === 4" />
         </n-icon>
         {{ item.title }}
-        <n-icon size="18">
+        <n-icon size="18" class="invisible group-hover:visible">
           <ChevronRight />
         </n-icon>
       </li>
