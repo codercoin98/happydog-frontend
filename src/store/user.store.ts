794eb13d178edbc6695dfff259964e50a098f61d
chenbin.li <lichenbin1119@gmail.com>
2022-09-29 17:01:12 +0800

    chore:添加用户store的state属性

--- src/store/user.store.ts
+++ src/store/user.store.ts
@@ -9,6 +9,10 @@ interface UserState {
     username: string
     nickname: string
     avatar_url: string
+    mood: string
+    bio: string
+    following: number
+    follower: number
     created_at: string
   } | null
 }
