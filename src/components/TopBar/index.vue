4e353950ca998414c19bff0477dc4ba0e62ff2dd
chenbin.li <lichenbin1119@gmail.com>
2022-10-18 11:15:54 +0800

    chore:抽离封装topbar组件

---
+++ src/components/TopBar/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="relative flex justify-center items-center">
+        <span
+            class="absolute left-0 flex items-center justify-center p-1 rounded hover:cursor-pointer hover:bg-gray-200  hover:text-purple-400"
+            @click="router.back()">
+            <n-icon size="20">
+                <ChevronLeft />
+            </n-icon>
+        </span>
+        <span class="lg:text-lg md:text-sm font-bold">
+            <slot name="title"></slot>
+        </span>
+        <div class="absolute right-0">
+            <slot name="right"></slot>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { ChevronLeft } from '@vicons/fa';
+const router = useRouter()
+</script>
+
+<style scoped>
+
+</style>
